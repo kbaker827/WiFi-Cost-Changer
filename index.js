23712fa32b0c2c91f856a7f5b1d6d8de12c10f05
CharukaHS <Charuka12@gmail.com>
2018-08-25 01:09:04 +0530

    Check state after windows wakeup :beers:

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const cmd = require('node-cmd')
 const schedule = require('node-schedule')
 const WindowsToaster = require('node-notifier').WindowsToaster
-
 const notifier = new WindowsToaster()
 
 const toUnrestricted = () => {
   cmd.get(`netsh wlan set profileparameter name="4G Router" cost=Unrestricted`,
   (err, data) => {
     if(err) throw err
-    console.log('WiFi cost changed to UNRESTRICTED')
-    notify(false)
+    notify('unrestricted')
   })
 }
 
@@ -17,8 +15,7 @@ const toFixed = () => {
   cmd.get(`netsh wlan set profileparameter name="4G Router" cost=Fixed`,
   (err, data) => {
     if(err) throw err
-    console.log('WiFi cost changed to FIXED')
-    notify(true)
+    notify('fixed')
   })
 }
 
@@ -31,8 +28,13 @@ const onStart = () => {
   }
 }
 
-const notify = (params) => {
-  let state = params ? 'metered' : 'unrestricted'
+const logger = (state) => {
+  let d = new Date()
+  console.log(`${d.getMonth()}${d.getDate()}/${d.getHours()}:${d.getMinutes()} WiFi cost changed to ${state}`)  
+}
+
+const notify = (state) => {
+  logger(state)
   notifier.notify({
     title: 'WiFi Cost Changed',
     message: `Connection changed to ${state} state`,
@@ -46,3 +48,14 @@ const notify = (params) => {
 onStart()
 schedule.scheduleJob('0 0 * * *', toUnrestricted)
 schedule.scheduleJob('0 8 * * *', toFixed)
+
+// Fire the a quick time check when windows wakeup after sleep/hibernation
+let lastTime = new Date().getTime()
+const timeOut = 2000
+setInterval(()=>{
+  let currentTime = new Date().getTime()
+  if (currentTime > lastTime + timeOut + 10000) {
+    onStart()
+  }
+  lastTime = currentTime
+}, timeOut)
